6f2590f18f30c361d43dd5db30a1cd8168972e9c
영근맥 <dudrmshub@gmail.com>
2019-03-31 23:45:40 +0900

    check rendering

--- _common.R
+++ _common.R
@@ -12,20 +12,13 @@ knitr::opts_chunk$set(
 
 # tidyverse -----------------------------------
 library(tidyverse)
-# tools for r markdown -------------------------
+# tools for r markdown ------------------------
 library(rmdtool)
-# textbook dataset package ---------------------
-library(MPV)
+# assistant tools for ggplot ------------------
+library(goodgraphic)
 
 # rounding digits ------------------------------
 options(digits = 3)
 
 # pander table remove NA ----------------------
 pander::panderOptions("missing", "")
-
-# ggplot default color -----------------------
-gg_hcl <- function(n) {
-  hue <- seq(15, 375, length.out = n + 1)
-  hcl_cols <- hcl(h = hue, c = 100, l = 65)[-(n + 1)]
-  hcl_cols
-}
